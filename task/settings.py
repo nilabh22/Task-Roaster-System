314dd3a82a1638e7b150db4f96f612542889ec4c
Nilabh Sahu <59756708+nilabh22@users.noreply.github.com>
2022-02-05 15:03:37 +0530

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -127,16 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prendowaliid@gmail.com' #email
-
-
-
-
-
-
-
-
-
-
-
-EMAIL_HOST_PASSWORD = 'ysfzihrzssoadsbi'  #password
+EMAIL_HOST_USER = '' #email
+EMAIL_HOST_PASSWORD = ''  #password
